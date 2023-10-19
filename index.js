e5b05942d4da08eb839925b6f7ba34330fb2d452
Tin Suhartini <girtiandalubis@gmail.com>
2023-10-19 11:11:16 +0700

    inisialisasi

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ const express = require('express')
 
     const app = express()
     const port = 5986
-    
-    app.get("/Girr/:id", (req, res) => {
-        res.send("Hallo saya Girtianda");
+
+    app.get("/Girr", (req, res) => {
+        res.json({
+            message: "Hallo ini Girtianda",
+        });
     });
-    app.post("/Girr/:id", (req, res) => {
-        res.send("Pengen makan lapar");
+
+    app.post("/Girr", (req, res) => {
+        res.json({
+            message: "Hallo ini response Gian",
+        });
     });
-    app.put("/Girr/:id", (req, res) => {
-        res.send("Mie ayam enak pisan");
+
+    app.put("/Girr", (req, res) => {
+        res.json({
+            message: "Pengen mie ayam gratis",
+        });
     });
-    app.delete("/Girr/:id", (req, res) => {
-        res.send("Beli mie ayam di cimahi");
+
+    app.delete("/Girr", (req, res) => {
+        res.json({
+            message: "mie ayam cimahi ngeunah pisan",
+        });
     });
 
     app.listen(port,() => console.log(`Server running on port ${port}`))
