d4454f3625fcbb3668b3efc1d3e4cc95129ea782
Tin Suhartini <girtiandalubis@gmail.com>
2023-10-19 11:27:34 +0700

    inisialisasi

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const express = require('express')
     const app = express()
     const port = 5986
 
+    const data_users = [
+        { id: 1, name: "Girtianda", alamat: "Batam" },
+        { id: 2, name: "Yadi ", alamat: "Bandung" },
+        { id: 3, name: "Sumanto", alamat: "Madura" },
+        { id: 4, name: "Agus", alamat: "Subang" },
+    ];
+
+    app.get("/Girr", (req, res) => {
+        const data = data_users;
+
+        let result = {
+            status: 200,
+            data: data,
+        };
+
+        res.json(result);
+    });
     app.get("/Girr", (req, res) => {
         res.json({
             message: "Hallo ini Girtianda",
